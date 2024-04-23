ec3081c78c5a95d4494e65dc21a3a9a7d5bc760e
rabnmgr7 <rabnmgr7@gmail.com>
2024-04-23 16:38:26 +0545

    db tag

--- scripts/img-build.sh
+++ scripts/img-build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker build -t rabnmgr7/employee-care:latest -t rabnmgr7/employee-care:$BUILD_NUMBER .
-docker build -t rabnmgr7/employee-care-db:latest -t rabnmgr7/employee-care:$BUILD_NUMBER ./sql/
+docker build -t rabnmgr7/employee-care-db:latest -t rabnmgr7/employee-care-db:$BUILD_NUMBER ./sql/
